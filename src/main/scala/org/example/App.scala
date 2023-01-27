f244179b4077ca4cd668fc10f5382d0225a296cf
arkadipb <alwaysgetarkadip1994@gmail.com>
2023-01-28 00:19:22 +0530

    modularise code

--- src/main/scala/org/example/App.scala
+++ src/main/scala/org/example/App.scala
@@ -1,9 +1,7 @@
 package org.example
 
-/**
- * @author ${user.name}
- */
 
+import org.apache.spark
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql
 import org.apache.spark.sql.Row
@@ -12,10 +10,41 @@ import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.expressions.Window.partitionBy
+import org.apache.spark.sql.DataFrame
 
 object NewDay {
 
   System.setProperty("hadoop.home.dir", "C:\\Users\\alway\\IdeaProjects\\Hadoop\\hadoop-3.2.1\\hadoop-2.7.1\\bin")
+
+
+  def execute_movieRatings(moviesRenamed: DataFrame, ratingsRenamed: DataFrame): (DataFrame) = {
+
+    val movieRatings = moviesRenamed.join(ratingsRenamed, Seq("movie_id"))
+      .groupBy("movie_id", "title")
+      .agg(
+        min("rating").as("min_rating"),
+        max("rating").as("max_rating"),
+        avg("rating").as("avg_rating")
+      )
+
+    movieRatings.show()
+
+    movieRatings
+  }
+
+  def execute_userRatings(moviesRenamed: DataFrame, ratingsRenamed: DataFrame): (DataFrame) = {
+
+
+    val userRatings = moviesRenamed.join(ratingsRenamed, Seq("movie_id"))
+      .groupBy("movie_id", "title", "genre", "user_id", "rating")
+      .agg(max("timestamp").as("timestamp"))
+      .withColumn("row_num", row_number.over(partitionBy("user_id").orderBy(col("rating").desc, col("timestamp"))))
+      .filter(col("row_num") <= 3)
+
+    userRatings.show()
+
+    userRatings
+  }
 
   def main(args : Array[String]) {
     val spark = SparkSession.builder()
@@ -37,66 +66,46 @@ object NewDay {
       .format("csv")
       .load()
 
-
-     val moviesRenamed = movies.withColumnRenamed("_c0","movie_id")
-      .withColumnRenamed("_c1","title")
-      .withColumnRenamed("_c2","genre")
+    val moviesRenamed = movies.withColumnRenamed("_c0", "movie_id")
+      .withColumnRenamed("_c1", "title")
+      .withColumnRenamed("_c2", "genre")
 
     val ratingsRenamed = ratings.withColumnRenamed("_c0", "user_id")
       .withColumnRenamed("_c1", "movie_id")
       .withColumnRenamed("_c2", "rating")
       .withColumnRenamed("_c3", "timestamp")
 
+    val movieRatings = execute_movieRatings(moviesRenamed, ratingsRenamed)
 
-    //moviesRenamed.show()
-    //ratingsRenamed.show()
-
-    val movieRatings = moviesRenamed.join(ratingsRenamed, Seq("movie_id"))
-      .groupBy("movie_id","title")
-      .agg(
-        min("rating").as("min_rating"),
-        max("rating").as("max_rating"),
-        avg("rating").as("avg_rating")
-      )
-
-    movieRatings.show()
-
-    val userRatings = moviesRenamed.join(ratingsRenamed, Seq("movie_id"))
-      .groupBy("movie_id","title","genre","user_id","rating")
-      .agg(max("timestamp").as("timestamp"))
-      .withColumn("row_num", row_number.over(partitionBy("user_id").orderBy(col("rating").desc, col("timestamp"))))
-      .filter(col("row_num") <= 3)
-
-    userRatings.show()
-
+    val userRatings = execute_userRatings(moviesRenamed, ratingsRenamed)
 
     movies.write
       .option("path","C:\\Users\\alway\\IdeaProjects\\data\\output\\movies\\")
       .option("header",true)
       .format("parquet")
       .mode("overwrite")
-      .save()//AsTable("movies")
+      .saveAsTable("movies")
 
-      ratings.write
+    ratings.write
       .option("path", "C:\\Users\\alway\\IdeaProjects\\data\\output\\ratings\\")
         .option("header",true)
       .format("parquet")
       .mode("overwrite")
-      .save() //AsTable("movies")
+      .saveAsTable("movies")
 
     movieRatings.write
       .option("path", "C:\\Users\\alway\\IdeaProjects\\data\\output\\movieRatings\\")
       .option("header", true)
       .format("parquet")
       .mode("overwrite")
-      .save() //AsTable("movies")
+      .saveAsTable("movies")
 
     userRatings.write
       .option("path", "C:\\Users\\alway\\IdeaProjects\\data\\output\\userRatings\\")
       .option("header",true)
       .format("parquet")
       .mode("overwrite")
-      .save() //AsTable("movies")
+      .saveAsTable("movies")
   }
 
 }
